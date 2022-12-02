7f6767c263288e8f7e4b4c8e0b8aaa03dea4d4e4
kisagge <kisagge@naver.com>
2022-12-02 17:47:24 +0900

    pagination maxPost 값 수정

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,11 +1,76 @@
 const Post = require("../models/postModel");
 const mongoose = require("mongoose");
 
+// Post Paging
+const paging = (page, totalPost) => {
+  const maxPost = 5;
+  const maxPage = 10;
+  let currentPage = page ? parseInt(page) : 1;
+  const hidePost = page === 1 ? 0 : (page - 1) * maxPost;
+  const totalPage = Math.ceil(totalPost / maxPost);
+
+  if (currentPage > totalPage) {
+    currentPage = totalPage;
+  }
+
+  const startPage = Math.floor((currentPage - 1) / maxPage) * maxPage + 1;
+  let endPage = startPage + maxPage - 1;
+
+  if (endPage > totalPage) {
+    endPage = totalPage;
+  }
+
+  return { startPage, endPage, hidePost, maxPost, totalPage, currentPage };
+};
+
 // Get all posts
 const getPosts = async (req, res) => {
-  const posts = await Post.find({}).sort({ createdAt: -1 });
+  const keyword = req.query.keyword ?? "";
+  const page = req.query.page ?? 1;
 
-  res.status(200).json(posts);
+  const regex = (pattern) => new RegExp(`.*${pattern}.*`);
+  const keywordRegex = regex(keyword);
+
+  try {
+    const totalPost = await Post.find({
+      $or: [
+        { name: { $regex: keywordRegex, $options: "i" } },
+        { description: { $regex: keywordRegex, $options: "i" } },
+      ],
+    }).countDocuments({});
+    if (!totalPost) {
+      throw Error();
+    }
+    let { startPage, endPage, hidePost, maxPost, totalPage, currentPage } =
+      paging(page, totalPost);
+    const posts = await Post.find({
+      $or: [
+        { name: { $regex: keywordRegex, $options: "i" } },
+        { description: { $regex: keywordRegex, $options: "i" } },
+      ],
+    })
+      .sort({
+        createdAt: -1,
+      })
+      .skip(hidePost)
+      .limit(maxPost);
+    const result = {
+      posts,
+      startPage,
+      endPage,
+      totalPage,
+      currentPage,
+    };
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(200).json({
+      posts: [],
+      startPage: 1,
+      endPage: 1,
+      totalPage: 1,
+      currentPage: 1,
+    });
+  }
 };
 
 // Get a single post
